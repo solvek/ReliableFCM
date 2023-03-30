bb2b4552728f2312d98788b8fbac3a0827a9482f
solvek <adamchuk@gmail.com>
2023-03-30 16:23:39 +0300

    Making request to simple page. Logging web request stages.

--- app/src/main/java/com/solvek/reliablefcm/FcmService.kt
+++ app/src/main/java/com/solvek/reliablefcm/FcmService.kt
@@ -1,15 +1,15 @@
 package com.solvek.reliablefcm
 
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import com.solvek.reliablefcm.utils.formatTime
 import timber.log.Timber
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
 
 class FcmService : FirebaseMessagingService() {
-    private val firestore = Firebase.firestore
-
     override fun onNewToken(token: String) {
         super.onNewToken(token)
         Timber.tag(TAG).d("Firebase token:$token")
@@ -20,22 +20,36 @@ class FcmService : FirebaseMessagingService() {
         val data = message.data
         val reliableId = data["reliableId"]
         val time = reliableId!!.toLong().formatTime()
-        Timber.tag(TAG).i("Received a message (id $reliableId, type ${data["type"]} time $time priority ${message.priority})")
-        uploadPush(reliableId)
+        Timber.tag(TAG).i("Received a push (id $reliableId, type ${data["type"]} time $time priority ${message.priority})")
+        if (data.getBool("network", true)){
+            networkOperation()
+        }
     }
 
-    private fun uploadPush(reliableId: String?) {
-        val now = System.currentTimeMillis()
-        val push = hashMapOf(
-            "timestamp" to now,
-            "reliableId" to reliableId,
-            "time" to now.formatTime()
-        )
+    private fun networkOperation(){
+        Timber.tag(TAG).i("Making web request")
+        val url = URL("https://reliablefcm.web.app/simple.txt")
+        val con = url.openConnection() as HttpURLConnection
+        con.requestMethod = "GET"
+        val responseCode = con.responseCode
+        Timber.tag(TAG).i("Response Code :: $responseCode. Reading content")
+        if (responseCode != HttpURLConnection.HTTP_OK) {
+            return
+        }
+        val reader =
+            BufferedReader(InputStreamReader(con.inputStream))
+        var inputLine: String?
+        val response = StringBuffer()
+        while (reader.readLine().also { inputLine = it } != null) {
+            response.append(inputLine)
+        }
+        reader.close()
+        Timber.tag(TAG).i("Data is read. Content '$response'")
+    }
 
-        firestore.collection("pushes").document(now.toString())
-            .set(push)
-            .addOnSuccessListener { Timber.tag(TAG).i("Uploaded push info for $reliableId") }
-            .addOnFailureListener { e -> Timber.tag(TAG).e(e, "Upload failed") }
+    private fun Map<String, String>.getBool(key: String, default: Boolean): Boolean {
+        val v = this[key] ?: return default
+        return v.equals("true", ignoreCase = true)
     }
 
     companion object {
